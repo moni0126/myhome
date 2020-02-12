671dc7d985136aeed60e87fdff945d05bade9606
moni0126 <59993927+moni0126@users.noreply.github.com>
2020-02-12 16:51:15 +0900

    Add files via upload

---
+++ Door2.java
@@ -0,0 +1,17 @@
+
+public class Door {
+	String name;
+
+	public Door(String name) {
+		this.name = name;
+	}
+
+	void open() {
+		　　System.out.println(name + "ドアが開きます。");
+
+    void close() {
+	    　　System.out.println(name + "ドアが閉めます。");
+ }
+
+}
+}
